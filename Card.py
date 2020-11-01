f3877ab25ae348966d4ba96ac162fdf126255df1
bgm <ninjanerd2781@gmail.com>
2020-11-01 10:09:52 -0800

    Update Card.py
    Added model and price to the class.

--- Card.py
+++ Card.py
@@ -1,8 +1,10 @@
 class Card:
-    def __init__(self, name, url, btnText):
+    def __init__(self, model, price, name, url, btnText):
         self.name = name
+        self.model = model
+        self.price = price
         self.url = url
-        self.btnText = btnText 
+        self.btnText = btnText
 
     def getName(self):
         return self.name
@@ -12,3 +14,9 @@ class Card:
 
     def getButtonText(self):
         return self.btnText
+
+    def getModel(self):
+        return self.model
+
+    def getPrice(self):
+        return self.price
